de0631c0fd99a593713a62e87f618edbc8d22695
Daniel Osuoha <dan646655@gmail.com>
2026-01-30 20:23:36 -0600

    Initial Commit

---
+++ SafeTone/SafeToneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeToneApp.swift
+//  SafeTone
+//
+//  Created by Daniel Osuoha on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct SafeToneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
